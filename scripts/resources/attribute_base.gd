f41d09230f5b0c60f2af5f21d54f23fa46ea8ea7
HaHAjax <128398800+HaHAjax@users.noreply.github.com>
2025-05-31 08:51:35 -0600

    refactoring yet again

--- scripts/resources/attribute_base.gd
+++ scripts/resources/attribute_base.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Resource
 class_name AttributeBase
 ## Base class for player attributes. This class is used to define attributes that can be added to the player.
 
--- scripts/resources/enemy_data.gd
+++ scripts/resources/enemy_data.gd
@@ -4,3 +4,4 @@ class_name EnemyData
 @export var sprite: Texture2D
 @export var collision_shape: RectangleShape2D
 @export var collision_shape_offset: Vector2 = Vector2.ZERO
+@export var attributes: Array[AttributeBase] = []
